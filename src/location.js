6ebe6d0a7fef772f3e73259cb61f50f456b750ad
Dave Lesage <dave.lesage@interstates.com>
2014-04-24 11:27:16 -0500

    updated location to be more consistent with other class declarations, leaving location as a single class declaration on the library

--- src/location.js
+++ src/location.js
@@ -7,12 +7,13 @@
 (function(w, undefined) {
     var z = w.util || {};
 
-    var location = {};
-    z.defineProperty(location, "parameters", {
-        get: function() { return getParameters() }, // automatically re-search for query parameters when accessing
-        writeable: false
-    });
+    /**
+        A method used by the location.parameters property which builds the 
+        window.location.href query parameters into an object containing key value pairs.
 
+        @returns {object} The object containing query parameter key value pairs.
+    */
+    var _regexPlus = /\+/g; // define once
     var getParameters = function() {
         var params = {};
         var href = window.location.href;
@@ -22,11 +23,26 @@
             for (var i = 0; i < queries.length; i++) {
                 var query = queries[i].split("=");
                 if (query.length != 2) continue;
-                params[query[0]] = decodeURIComponent(query[1].replace(/\+/g, " "));
+                params[query[0]] = decodeURIComponent(query[1].replace(_regexPlus, " "));
             }
         }
         return params;
-    }
+    };
+
+    /**
+        An interface class used with the window.location object.
+        Note that the provided log interface is expected to contain at least
+        a debug, error, info, log, and warn method.
+
+        @class Contains a window.location interface.
+     */
+    var location = (function(locationObj) {
+        z.defineProperty(locationObj, "parameters", {
+            get: function() { return getParameters() }, // automatically re-search for query parameters when accessing
+            writeable: false
+        });
+        return locationObj;
+    })({});
 
     z.location = location;
     w.util = z;
